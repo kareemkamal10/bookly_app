aecf3eb4ba2dad963bac5873f44fb5cd1556d349
kareemkamal10 <kareemkamal500@outlook.com>
2025-05-28 16:41:04 +0300

    feat: Implement fetchFeaturedBooks method and add error logging in HomeRemoteDataSourceImpl

--- lib/Features/home/data/sources/home_remote_data_sourc.dart
+++ lib/Features/home/data/sources/home_remote_data_sourc.dart
@@ -1,4 +1,7 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/helper/log_helper.dart';
+import 'package:bookly/core/network/api_service.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -6,10 +9,26 @@ abstract class HomeRemoteDataSource {
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    final data = await apiService.get(
+        endPoint: '/volumes?Filtering=free-ebooks&q=computer science');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic>? data) {
+    List<BookEntity> books = [];
+    if (data == null || data['items'] == null) {
+      logError('Error fetching featured books: No data or items found');
+    } else {
+      for (var bookMap in data['items']) {
+        books.add(BookModel.fromJson(bookMap));
+      }
+    }
+    return books;
   }
 
   @override
