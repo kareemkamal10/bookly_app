2e4375c9fa393b4a545a4fbd253e88e45d6c7279
kareemkamal10 <kareemkamal500@outlook.com>
2025-05-29 14:23:11 +0300

    fix: Update import statements for FetchFeatureBooksCubit and FetchNewsBooksCubit to use flutter_bloc package

--- lib/Features/home/presentation/views/controller/fetc_feature_books/fetch_feature_books_cubit.dart
+++ lib/Features/home/presentation/views/controller/fetc_feature_books/fetch_feature_books_cubit.dart
@@ -1,6 +1,6 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'fetch_feature_books_state.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/Features/home/data/sources/home_loacl_data_source.dart';
+import 'package:bookly/Features/home/data/sources/home_remote_data_sourc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/Features/home/presentation/views/controller/fetc_feature_books/fetch_feature_books_cubit.dart';
 import 'package:bookly/core/consts/constants.dart';
+import 'package:bookly/core/network/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
@@ -19,12 +27,30 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     debugInvertOversizedImages = true; // For debugging oversized images
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FetchFeatureBooksCubit(
+            FetchFeaturedBooksUseCase(
+              HomeRepoImpl(
+                homeLocalDataSource: HomeLocalDataSourceImpl(),
+                homeRemoteDataSource: HomeRemoteDataSourceImpl(
+                  ApiService(
+                    Dio(),
+                  ),
+                ),
+              ),
+              )
+            )
+          ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
